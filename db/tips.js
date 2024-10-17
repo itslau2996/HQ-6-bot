1dedcc835ec074a51eb5faf0a765de04d9b96d55
Laurens <wezellaurens@gmail.com>
2024-10-17 08:43:55 +0200

    Start-up

--- db/tips.js
+++
@@ -1,5 +0,0 @@
-const tipsArray = [
-    'Wist je dat? je een account aan kunt maken binnen deze bot!\nDit doe je met `/account`',
-    'Met `/muzieklink` krijg je een stappenplan om je favoriete nummer toe te voegen!',
-    'Met `/profielopties` zijn er extra dingetjes beschikbaar om aan je profiel toe te voegen!',
-];
