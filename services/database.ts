df68f0a3d5b6ce86c9eb8ab4385a68dd22ab12fc
Laurens <wezellaurens@gmail.com>
2024-10-20 19:53:09 +0200

    beginning of getService

---
+++ services/database.ts
@@ -0,0 +1,6 @@
+interface props {
+    table: "projectUserHistory" | "accountsJoin" | "reservations" | "accounts" | "projects" | "devices" | "notificationScheduling" | "resetTablesAt" | "nodeScheduling" | "counts" | "users";
+}
+export function getDatabaseData({ table }: props) {
+
+}
